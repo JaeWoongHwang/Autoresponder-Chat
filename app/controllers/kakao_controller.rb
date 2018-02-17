02949b06a01b7ea9536583a7225a220f058d5715
JaeWoong Hwang <jwngh15@gmail.com>
2018-02-17 12:28:44 +0000

    Second Commit

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,12 +1,28 @@
 class KakaoController < ApplicationController
   def keyboard
    @keyboard = {
-      :type => "buttons",						
-      buttons: ["선택 1", "선택 2", "선택 3"]		 
+      :type => "text"
     }
     render json: @keyboard
   end
 
   def message
+   @user_msg = params[:message]
+   @message = @user_msg
+   
+   @message = {
+     text: @msg
+   }
+   
+   @standard_keyboard = {
+     :type => "text"
+   }
+   
+   @result = {
+     message: @msg,
+     keyboard: @standard_keyboard
+   }
+   
+   render json: @result
   end
 end
